b9aa9cd7e43c2d7056d0e4dee118acbbf011fbce
tanaka <126080524+TheDritROOK@users.noreply.github.com>
2023-03-18 00:22:50 +0800

    3.17学习日志
    while语句学习，GitHub的PR处理失败，重新建立公开仓库。

---
+++ 317/study/317/3.17.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+int main()
+{
+	int line = 0;
+	while (line < 30000)
+	{
+		printf("Ð´´úÂë\n");
+		line = line++;
+	}
+	if (line = 30000)
+	{
+		printf("all right\n");
+	}
+
+	return 0;
+}
